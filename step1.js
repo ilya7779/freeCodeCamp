f4886a8e369754dcaf51d0af3b278f2323614b1c
ilya7779 <ilya.makota01@gmail.com>
2024-09-01 21:34:04 +0200

    solved task 'step1'

---
+++ step1.js
@@ -0,0 +1,7 @@
+console.log("Let's build a pyramid using JavaScript basics:");
+console.log("     #     ");
+console.log("    ###    ");
+console.log("   #####   ");
+console.log("  #######  ");
+console.log(" ######### ");
+console.log("###########");
